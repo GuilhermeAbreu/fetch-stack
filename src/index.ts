e25b94a4cff160547f312a391ad61018846bb4e2
Guilherme Abreu <guilherme.abreu2019@gmail.com>
2025-05-07 19:53:02 -0300

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-
-export { DataManager } from './DataManager';
-export { DataOperation } from './DataOperation';
-export { FocusManager } from './FocusManager';
-export { MutationObserver } from './MutationObserver';
-export { NetworkManager } from './NetworkManager';
-export { QueryClient } from './QueryClient';
-export { usePaginatedQuery } from './usePaginatedQuery';
-export { useQuery } from './useQuery';
+export { DataManager } from "./DataManager";
+export { DataOperation } from "./DataOperation";
+export { FocusManager } from "./FocusManager";
+export { MutationObserver } from "./MutationObserver";
+export { NetworkManager } from "./NetworkManager";
+export { QueryClient } from "./QueryClient";
+export { usePaginatedQuery } from "./usePaginatedQuery";
+export { useQuery } from "./useQuery";
 
 export type {
-  DataFetchOptions, FetchStatus, MutationOptions, MutationState, NetworkMode, OperationOptions, OperationState, PaginatedData,
-  PaginatedQueryOptions, Query, QueryKey,
+  DataFetchOptions,
+  FetchStatus,
+  MutationOptions,
+  MutationState,
+  NetworkMode,
+  OperationOptions,
+  OperationState,
+  PaginatedData,
+  PaginatedQueryOptions,
+  Query,
+  QueryKey,
   QueryOptions,
-  QueryState, QueryStatus
-} from './types';
-
+  QueryState,
+  QueryStatus,
+} from "./types";
